6aaec998508f9cb74cea1e3cdfd2af2234f0e98b
ShaneOH <shane.ohanlon93@gmail.com>
2018-04-09 02:53:46 +0700

    convert tables to postgres

--- api/tables.py
+++ api/tables.py
@@ -1,18 +1,8 @@
 #!/usr/bin/env python3
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy.dialects.mysql import DOUBLE
 from flask import jsonify
 import enum
 db = SQLAlchemy()
-
-class DayEnum(enum.Enum):
-    Sun = 0
-    Mon = 1
-    Tue = 2
-    Wed = 3
-    Thu = 4
-    Fri = 5
-    Sat = 6
 
 class Table:
     def to_json(self):
@@ -29,16 +19,13 @@ class Table:
 
 class ApTransactions(Table, db.Model):
     __tablename__ = 'ap_transactions'
-    id = db.Column(db.BIGINT, primary_key=True)
+    id = db.Column(db.Integer, primary_key=True)
     ticket = db.Column(db.Integer)
     pay_station = db.Column(db.VARCHAR(length=50))
     stall = db.Column(db.VARCHAR(length=50))
     license_plate = db.Column(db.VARCHAR(length=8))
-    day = db.Column(db.Enum(DayEnum))
-    date = db.Column(db.DATE)
-    time = db.Column(db.TIME)
-    purchased_date = db.Column(db.DATETIME, nullable=False)
-    expiry_date = db.Column(db.DATETIME)
+    purchased_date = db.Column(db.TIMESTAMP, nullable=False)
+    expiry_date = db.Column(db.TIMESTAMP)
     payment_type = db.Column(db.VARCHAR(length=50))
     transaction_type = db.Column(db.VARCHAR(length=50))
     coupon_code = db.Column(db.VARCHAR(length=50))
@@ -50,10 +37,10 @@ class ApTransactions(Table, db.Model):
     rate_name = db.Column(db.VARCHAR(length=50))
     hours_paid = db.Column(db.DECIMAL(precision=4, scale=2))
     zone = db.Column(db.VARCHAR(length=50))
-    new_rate_weekday = db.Column(DOUBLE)
-    new_revenue_weekday = db.Column(DOUBLE)
-    new_rate_weekend = db.Column(DOUBLE)
-    new_revenue_weekend = db.Column(DOUBLE)
+    new_rate_weekday = db.Column(db.DECIMAL(precision=10, scale=2))
+    new_revenue_weekday = db.Column(db.DECIMAL(precision=10, scale=2))
+    new_rate_weekend = db.Column(db.DECIMAL(precision=10, scale=2))
+    new_revenue_weekend = db.Column(db.DECIMAL(precision=10, scale=2))
     passport_tran = db.Column(db.Integer)
     merchant_tran = db.Column(db.BIGINT)
     parker_id = db.Column(db.Integer)
